f5bdfc13ae2f26689509f167afc89347caba4567
anchel <anchel.xy@gmail.com>
2024-07-18 19:02:46 +0800

    feat: update

---
+++ src/views/OtherView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="other">
+    <h1>This is an other page</h1>
+    <van-form @submit="onSubmit">
+      <van-cell-group inset>
+        <van-field
+          v-model="username"
+          name="username"
+          label="用户名"
+          placeholder="用户名"
+          :rules="[{ required: true, message: '请填写用户名' }]"
+        />
+        <van-field
+          v-model="password"
+          type="password"
+          name="password"
+          label="密码"
+          placeholder="密码"
+          :rules="[{ required: true, message: '请填写密码' }]"
+        />
+      </van-cell-group>
+      <div style="margin: 16px">
+        <van-button round block type="primary" native-type="submit"> 提交 </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+import md5 from 'js-md5'
+
+const username = ref('')
+const password = ref('')
+
+const onSubmit = async (params) => {
+  console.log('onSubmit', params)
+  params.password = md5(params.password)
+  let response = await axios.post('/api/login', params, {})
+
+  console.log('ret', response.data)
+}
+</script>
+
+<style lang="less" scoped>
+.other {
+  padding: 10px;
+}
+</style>
